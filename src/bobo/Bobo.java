12b4d17617a188625b39e15c13639d9655817bea
mozict <mozict@mozict-Lenovo-G470>
2017-12-20 10:21:35 +0700

    sfs

---
+++ src/bobo/Bobo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bobo;
+
+/**
+ *
+ * @author mozict
+ */
+public class Bobo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
